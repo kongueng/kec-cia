2d3d0c6e680ba89a15955659842fc6219a913247
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2014-07-27 23:35:43 +0530

    change in db connectivity params

--- db/db.php
+++ db/db.php
@@ -1,10 +1,10 @@
 <?php
-/*
+//this file gets db connectivity information from the OpenShift env automatically.
 $db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
 $db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
 $db_password = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
 $db_name = $_ENV['OPENSHIFT_APP_NAME'];
-$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];*/
-$con = mysqli_connect("localhost", "root", "", "cia", "3306");
+$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
+$con = mysqli_connect($db_host, $db_user, $db_password, $db_name, $db_port);
 if(!$con){die("Failed to connect to MySQL");}
 ?>
