3d702624bdadb155960344204b1bcb13ac9e1a2c
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2014-07-27 22:45:18 +0530

    database status

--- db/db.php
+++ db/db.php
@@ -1,9 +1,10 @@
 <?php
+/*
 $db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
 $db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
 $db_password = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
 $db_name = $_ENV['OPENSHIFT_APP_NAME'];
-$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
-$con = mysqli_connect($db_host, $db_user, $db_password, $db_name, $db_port);
+$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];*/
+$con = mysqli_connect("localhost", "root", "", "cia", "3306");
 if(!$con){die("Failed to connect to MySQL");}
 ?>
