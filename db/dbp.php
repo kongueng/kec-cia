03841cad740a9bccb759c42836dc39793189e5e5
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2014-07-28 14:24:27 +0530

    added prepared db connectivity

---
+++ db/dbp.php
@@ -0,0 +1,5 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "cia");
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
