92d4b298cb132f4a2f1a070061371bc1d14ddd00
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2014-07-28 14:30:33 +0530

    mod db params for dbp.php

--- db/dbp.php
+++ db/dbp.php
@@ -1,5 +1,10 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "cia");
+$db_host = $_ENV['OPENSHIFT_MYSQL_DB_HOST'];
+$db_user = $_ENV['OPENSHIFT_MYSQL_DB_USERNAME'];
+$db_password = $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD'];
+$db_name = $_ENV['OPENSHIFT_APP_NAME'];
+$db_port = $_ENV['OPENSHIFT_MYSQL_DB_PORT'];
+$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name);
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
