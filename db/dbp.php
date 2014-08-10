a2c1ac6d172d0ae8e113bb01e023028cd8f637c0
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2014-08-10 11:31:05 +0530

    added exit on failure

--- db/dbp.php
+++ db/dbp.php
@@ -2,4 +2,5 @@
 $mysqli = new mysqli("localhost", "root", "", "cia");
 if ($mysqli->connect_errno) {
 	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+        exit();
 }
